901eed74bfb08fa644ea5c59940964bc6cfb6b6f
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-06-29 15:42:27 +0530

    sending request with data to IJ

--- src/main/java/com/example/studentmanagement_aad/studentController.java
+++ src/main/java/com/example/studentmanagement_aad/studentController.java
@@ -1,4 +1,45 @@
 package com.example.studentmanagement_aad;
 
-public class studentController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/student")
+public class studentController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Todo: get student details
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        //Todo: save student
+        if (!req.getContentType().toLowerCase().contains("application/json")|| req.getContentType()==null){
+          //send error
+            resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+        }
+        //process
+        BufferedReader reader =req.getReader();
+        StringBuilder sb =new StringBuilder();
+        var writer =resp.getWriter();
+        reader.lines().forEach(line->sb.append(line+"\n"));
+        System.out.println(sb);
+        writer.write(sb.toString());
+        writer.close();
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         //Todo: update student
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Todo: delete student
+    }
 }
