9d0b7f22a092020646f95312443c46ff2a5eae1b
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-06-29 16:29:13 +0530

    sending request with data to IJ option 2

--- src/main/java/com/example/studentmanagement_aad/studentController.java
+++ src/main/java/com/example/studentmanagement_aad/studentController.java
@@ -1,5 +1,8 @@
 package com.example.studentmanagement_aad;
 
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,6 +11,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+
+import static jakarta.json.Json.createReader;
 
 @WebServlet(urlPatterns = "/student")
 public class studentController extends HttpServlet {
@@ -24,13 +29,20 @@ public class studentController extends HttpServlet {
             resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
         }
         //process
-        BufferedReader reader =req.getReader();
-        StringBuilder sb =new StringBuilder();
-        var writer =resp.getWriter();
-        reader.lines().forEach(line->sb.append(line+"\n"));
-        System.out.println(sb);
-        writer.write(sb.toString());
-        writer.close();
+//        BufferedReader reader =req.getReader();
+//        StringBuilder sb =new StringBuilder();
+//        var writer =resp.getWriter();
+//        reader.lines().forEach(line->sb.append(line+"\n"));
+//        System.out.println(sb);
+//        writer.write(sb.toString());
+//        writer.close();
+
+
+        //Json emulate with parson
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject =reader.readObject();
+        System.out.println(jsonObject.getString("email"));
+
     }
 
     @Override
